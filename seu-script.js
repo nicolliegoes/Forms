c5bf6988cb584f03b06501aac9eccebd21051163
Nicollie Machtura Goes <111029564+nicolliegoes@users.noreply.github.com>
2023-09-20 21:27:54 -0300

    Add files via upload

--- seu-script.js
+++ seu-script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $("#buscar-cep").click(function () {
         const cep = $("#cep").val();
 
-        // Fazer solicitação AJAX para o ViaCEP
         $.ajax({
             url: `https://viacep.com.br/ws/${cep}/json/`,
             dataType: "json",
@@ -25,16 +24,13 @@ $(document).ready(function () {
     document.getElementById("cadastro-form").addEventListener("submit", function (e) {
         e.preventDefault();
 
-        // Obtenha os dados do formulário
         const formData = new FormData(this);
 
-        // Crie um objeto para armazenar os dados do cadastro
         const cadastroData = {};
         formData.forEach((value, key) => {
             cadastroData[key] = value;
         });
 
-        // Armazene temporariamente os dados no SessionStorage
         sessionStorage.setItem("cadastroData", JSON.stringify(cadastroData));
 
     });
